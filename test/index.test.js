b21b387272e0837ed4a7a9ad28397d9739799f42
Kavita Makariye <kavitasmakariye@gmail.com>
2019-06-15 10:19:08 +0530

    [Restro 3] Setting build configurations for travis-CI (#3)
    * added build configuration using travis-ci
    
    * added travis badage of travis to readme.md file
    
    * changed the message in test file
    
    * changed filename to index.js

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const index = require('../index');
+
+//Sample test case to verify testing environment working
+//Todo: Replace sample test case with actual test case scenarios
+describe('test', () => {
+    it('Should return string', () => {
+        let result = 'ci with travis'
+        assert.equal(result,'ci with travis');
+    });
+});
