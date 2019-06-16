3c2c2606f8784c92407d9eee74e5659c283e4cc7
kavitamakariye <kavitasmakariye@gmail.com>
2019-06-16 18:20:00 +0530

    added route variable in server.js

--- js/server.js
+++ js/server.js
@@ -5,11 +5,12 @@ const logger = require('./logger').getLogger('application');
 
 const app = express();
 const port = nconf.get('server:port');
+const route = require('./routes');
 
 function start() {
    
     //Routes
-    app.use(require('./routes'))
+    app.use(route)
 
     app.listen(port, () => {
         logger.debug(`Listening on port: ${port}`);
